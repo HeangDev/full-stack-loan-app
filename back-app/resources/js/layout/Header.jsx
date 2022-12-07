e9ffc4960c072acc90ea9c8eb3a2288f99f8e83e
kimheangsim <simkimheang4@gmail.com>
2022-12-07 22:44:41 +0700

    Heang Update

--- back-app/resources/js/layout/Header.jsx
+++ back-app/resources/js/layout/Header.jsx
@@ -2,57 +2,46 @@ import React from 'react'
 import { BiMenuAltLeft } from "react-icons/bi";
 import { Link } from 'react-router-dom'
 import Avatar from '../assets/avatar.jpg'
+import { dropmenu } from '../data/dropmenu';
+import { Menu, Transition } from '@headlessui/react'
 
 const Header = () => {
-    const is_login = localStorage.getItem('is_login')
     return (
         <>
-            <div className="header">
-                <div className="left">
-                    <button type="button">
-                        <BiMenuAltLeft className="w-6 h-6 fill-gray-400"/>
-                    </button>
-                </div>
-                <div className="right">
-                    <div className="profile_avatar">
-                        <button className="profile_avatar_action">
-                            <img src={Avatar} alt="" />
-                        </button>
-                        {/* <div className="drop_menu">
-                            <Link to="">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                                </svg>
-                                <span>My Account</span>
-                            </Link>
-                            <Link to="">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
-                                </svg>
-                                <span>About</span>
-                            </Link>
-                            <Link to="">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
-                                </svg>
-                                <span>Support</span>
-                            </Link>
-                            <Link to="">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                                </svg>
-                                <span>Help</span>
-                            </Link>
-                            <Link to="">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                                </svg>
-                                <span>Logout</span>
-                            </Link>
-                        </div> */}
-                    </div>
-                </div>
-            </div>
+            <Menu as="div" className="header">
+                {({ open }) => (
+                    <>
+                        <div className="left">
+                            <button type="button">
+                                <BiMenuAltLeft className="w-6 h-6 fill-gray-400"/>
+                            </button>
+                        </div>
+                        <div className="right">
+                            <div className="profile_avatar">
+                                <Menu.Button className="profile_avatar_action">
+                                    <img src={Avatar} alt="" />
+                                </Menu.Button>
+                                <Transition show={open}>
+                                    <Menu.Items>
+                                        <div className="drop_menu">
+                                            {dropmenu.map(( item ) => {
+                                                return (
+                                                    <Menu.Item key={item.id}>
+                                                        <Link to={item.url}>
+                                                            <item.icon/>
+                                                            <span>{item.name}</span>
+                                                        </Link>
+                                                    </Menu.Item>
+                                                )
+                                            })}
+                                        </div>
+                                    </Menu.Items>
+                                </Transition>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </Menu>
         </>
     )
 }
