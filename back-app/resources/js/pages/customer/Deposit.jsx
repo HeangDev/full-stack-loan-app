22a2940171248ccbcbe2a1755d59630360981ef8
kimheangsim <simkimheang4@gmail.com>
2022-12-19 11:53:58 +0700

    Heang Update

--- back-app/resources/js/pages/customer/Deposit.jsx
+++ back-app/resources/js/pages/customer/Deposit.jsx
@@ -7,6 +7,11 @@ import axios from 'axios'
 import { AiOutlineEdit, AiOutlineDelete, AiOutlineClose } from "react-icons/ai";
 import { Dialog, Transition } from '@headlessui/react'
 
+import "jquery/dist/jquery.min.js";
+import "datatables.net-dt/js/dataTables.dataTables";
+import "datatables.net-dt/css/jquery.dataTables.css";
+import $ from "jquery";
+
 const Deposit = () => {
     const [editIsOpen, setEditIsOpen] = useState(false)
 
@@ -35,7 +40,7 @@ const Deposit = () => {
             setWithDrawCode(withdraw_code);
             setDescription(description);
             setIdDeposit(iddeposit);
-        }).catch(({err}) => {
+        }).catch(({ err }) => {
             console.log(err)
         })
     }
@@ -57,7 +62,7 @@ const Deposit = () => {
                 timer: '1500'
             })
             setEditIsOpen(false)
-        }).catch(({err}) => {
+        }).catch(({ err }) => {
             console.log(err)
         })
     }
@@ -79,7 +84,7 @@ const Deposit = () => {
             return;
         }
 
-        await axios.delete(`http://127.0.0.1:8000/api/deposit/${id}`).then(({data}) => {
+        await axios.delete(`http://127.0.0.1:8000/api/deposit/${id}`).then(({ data }) => {
             Swal.fire({
                 title: 'Success!',
                 text: "User has been deleted!",
@@ -87,64 +92,51 @@ const Deposit = () => {
                 timer: '1500'
             })
             fetchDeposit()
-        }).catch(({err}) => {
+        }).catch(({ err }) => {
             console.log(err)
         })
     }
 
     useEffect(() => {
         fetchDeposit()
-    }, [])
 
-    const columns = [
-        {
-            name: '#',
-            selector: row => row.id,
-        },
-        {
-            name: "รหัสถอนเงิน",
-            selector: (row) => 
-            row.withdraw_code === '' || row.withdraw_code === null ?
-            <>ไม่สมบูรณ์</>
-            :
-            <>{row.withdraw_code}</>,
-            sortable: true,
-        },
-        {
-            name: "จำนวนเงิน",
-            selector: (row) => 
-            row.deposit_amount === '' || row.deposit_amount === null ?
-            <>ไม่สมบูรณ์</>
-            :
-            <>{row.deposit_amount}</>,
-            sortable: true,
-        },
-        {
-            name: "ลักษณะ",
-            selector: (row) => 
-            row.description === '' || row.description === null ?
-            <>ไม่สมบูรณ์</>
-            :
-            <>{row.description}</>,
-            sortable: true,
-        },
-        {
-            name: "ลักษณะ",
-            selector: (row) => 
-            row.deposit_date === '' || row.deposit_date === null ?
-            <>ไม่สมบูรณ์</>
-            :
-            <>{row.deposit_date}</>,
-            sortable: true,
-        },
-        {
-            name: "ตัวเลือก",
-            cell: (row) => [
-                <button type="button" onClick={() => handleEdit(row.id)} className="btn_edit"><AiOutlineEdit/></button>,
-                <button type="button" onClick={() => handleDelete(row.id)} className="btn_delete"><AiOutlineDelete/></button>
-           ]
-        },
-    ]
+        if (!$.fn.dataTable.isDataTable("#tbldeposit")) {
+            $(document).ready(function () {
+                setTimeout(function () {
+                    $("#tbldeposit").DataTable({
+                        pageLength: 10,
+                        processing: true,
+                        select: {
+                            style: "single",
+                        },
+                        // "language": {
+                        //     "lengthMenu": "Display _MENU_ records per page",
+                        //     "zeroRecords": "Nothing found - sorry",
+                        //     "info": "Showing page _PAGE_ of _PAGES_",
+                        //     "infoEmpty": "No records available",
+                        //     "infoFiltered": "(filtered from _MAX_ total records)"
+                        // },
+
+                        fnRowCallback: function (
+                            nRow,
+                            aData,
+                            iDisplayIndex,
+                            iDisplayIndexFull
+                        ) {
+                            var index = iDisplayIndexFull + 1;
+                            $("td:first", nRow).html(index);
+                            return nRow;
+                        },
+
+                        lengthMenu: [
+                            [10, 20, 30, 50, -1],
+                            [10, 20, 30, 50, "All"],
+                        ],
+                    });
+                }, 1000);
+            });
+        }
+    }, [])
 
     return (
         <>
@@ -160,77 +152,98 @@ const Deposit = () => {
                     </div>
                     <div className="card_tbl_body">
                         <div className="tbl_scroll">
-                            <DataTable
-                                columns={columns}
-                                data={deposit}
-                                pagination
-                                keyField='id'
-                            />
+                            <table className="table table-striped dataTable" id="tbldeposit">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>รหัสถอนเงิน</th>
+                                        <th>จำนวนเงิน</th>
+                                        <th>ลักษณะ</th>
+                                        <th>ลักษณะ</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        deposit && deposit.length > 0 && (
+                                            deposit.map((item, index) => (
+                                                <tr key={index}>
+                                                    <td>{index + 1}</td>
+                                                    <td>{item.withdraw_code}</td>
+                                                    <td>{item.deposit_amount}</td>
+                                                    <td>{item.description}</td>
+                                                    <td>{item.deposit_date}</td>
+                                                </tr>
+                                            ))
+                                        )
+                                    }
+                                </tbody>
+                            </table>
+
                         </div>
                     </div>
                 </div>
 
                 <Transition appear show={editIsOpen} as={Fragment}>
                     <Dialog as="div" className="modal" onClose={() => setEditIsOpen(false)}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex items-center justify-center p-4 text-center">
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
-                            enterFrom="opacity-0 scale-95"
-                            enterTo="opacity-100 scale-100"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
                             leave="ease-in duration-200"
-                            leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
                         >
-                            <Dialog.Panel className="modal_dialog">
-                                <form autoComplete="off" onSubmit={handleUpdate}>
-                                    <div className="modal_header">
-                                        <Dialog.Title
-                                            as="h4"
-                                            className="modal_title"
-                                        >
-                                            แก้ไขยอดเติม
-                                        </Dialog.Title>
-                                        <button type="button" className="btn_close" onClick={() => setEditIsOpen(false)}><AiOutlineClose/></button>
-                                    </div>
-                                    <div className="modal_body">
-                                        <div className="frm_wrap">
-                                            <div className="frm_grp">
-                                                <label>รหัสถอนเงิน</label>
-                                                <input type="number" placeholder="รหัสถอนเงิน 4 หลัก" value={withdrawCode} onChange={(e) => {setWithDrawCode(e.target.value)}}  /> 
-                                            </div>
-                                            <div className="frm_grp">
-                                                <label>จำนวนเงิน</label>
-                                                <input type="number" placeholder="ระบุจำนวนเงินกู้ให้กับลูกค้า" value={depositAmount} onChange={(e) => {setDepositAmount(e.target.value)}} /> 
-                                            </div>
-                                            <div className="frm_grp">
-                                                <label>คำอธิบาย</label>
-                                                <textarea rows={2} value={description} onChange={(e) => setDescription(e.target.value)}/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <input type="hidden" value={idDeposit} onChange={(e) => {setIdDeposit(e.target.value)}} />
-                                    <div className="modal_footer">
-                                        <button type="button" className="btn_close" onClick={() => setEditIsOpen(false)}>ออกจาก</button>
-                                        <button type="submit" className="btn_save">ยืนยัน</button>
-                                    </div>
-                                </form>
-                            </Dialog.Panel>
+                            <div className="fixed inset-0 bg-black bg-opacity-25" />
                         </Transition.Child>
+                        <div className="fixed inset-0 overflow-y-auto">
+                            <div className="flex items-center justify-center p-4 text-center">
+                                <Transition.Child
+                                    as={Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 scale-95"
+                                    enterTo="opacity-100 scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 scale-100"
+                                    leaveTo="opacity-0 scale-95"
+                                >
+                                    <Dialog.Panel className="modal_dialog">
+                                        <form autoComplete="off" onSubmit={handleUpdate}>
+                                            <div className="modal_header">
+                                                <Dialog.Title
+                                                    as="h4"
+                                                    className="modal_title"
+                                                >
+                                                    แก้ไขยอดเติม
+                                                </Dialog.Title>
+                                                <button type="button" className="btn_close" onClick={() => setEditIsOpen(false)}><AiOutlineClose /></button>
+                                            </div>
+                                            <div className="modal_body">
+                                                <div className="frm_wrap">
+                                                    <div className="frm_grp">
+                                                        <label>รหัสถอนเงิน</label>
+                                                        <input type="number" placeholder="รหัสถอนเงิน 4 หลัก" value={withdrawCode} onChange={(e) => { setWithDrawCode(e.target.value) }} />
+                                                    </div>
+                                                    <div className="frm_grp">
+                                                        <label>จำนวนเงิน</label>
+                                                        <input type="number" placeholder="ระบุจำนวนเงินกู้ให้กับลูกค้า" value={depositAmount} onChange={(e) => { setDepositAmount(e.target.value) }} />
+                                                    </div>
+                                                    <div className="frm_grp">
+                                                        <label>คำอธิบาย</label>
+                                                        <textarea rows={2} value={description} onChange={(e) => setDescription(e.target.value)} />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <input type="hidden" value={idDeposit} onChange={(e) => { setIdDeposit(e.target.value) }} />
+                                            <div className="modal_footer">
+                                                <button type="button" className="btn_close" onClick={() => setEditIsOpen(false)}>ออกจาก</button>
+                                                <button type="submit" className="btn_save">ยืนยัน</button>
+                                            </div>
+                                        </form>
+                                    </Dialog.Panel>
+                                </Transition.Child>
+                            </div>
                         </div>
-                    </div>
                     </Dialog>
                 </Transition>
             </Layout>
